7bdb7dd540f4d73577d6dd0ea1c1c6dda879adb8
Untermensch <untermenschh@protonmail.com>
2025-02-19 22:29:03 +0300

    update vk schema, now without not null values

--- apps/backend/src/db/schema/user/vk-user.ts
+++ apps/backend/src/db/schema/user/vk-user.ts
@@ -3,20 +3,18 @@ import { createInsertSchema, createSelectSchema } from 'drizzle-zod'
 
 export const vkUser = pgTable('vk_user', {
   id: integer('id').primaryKey(),
-  first_name: varchar('first_name').notNull(),
-  last_name: varchar('last_name').notNull(),
-  sex: smallint('sex').notNull(), // 0, 1, or 2
-  city: varchar('city').notNull(), // JSON for { id, title }
-  country: varchar('country').notNull(), // JSON for { id, title }
-  bdate: varchar('bdate', { length: 255 }), // Optional string (e.g., "D.M.YYYY" or "D.M")
-  photo_100: varchar('photo_100').notNull(),
-  photo_200: varchar('photo_200').notNull(),
+  first_name: varchar('first_name'),
+  last_name: varchar('last_name'),
+  sex: smallint('sex'),
+  city: varchar('city'),
+  country: varchar('country'),
+  bdate: varchar('bdate',), 
+  photo_100: varchar('photo_100'),
+  photo_200: varchar('photo_200'),
   photo_max_orig: varchar('photo_max_orig'),
   timezone: smallint('timezone'),
 })
 
 export const selectVkUserSchema = createSelectSchema(vkUser)
 
-export const insertVkUserSchema = createInsertSchema(vkUser, {
-  id: (schema) => schema.id.positive(), // Ensure `id` is a positive integer
-})
+export const insertVkUserSchema = createInsertSchema(vkUser)
